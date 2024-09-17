523bb1fccd3d8b1d5cfec712b0861a7197b53835
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-09-17 19:26:19 +0900

    p166 失敗したテスト作成、コメントアウト（あと回し）

--- xunit.py
+++ xunit.py
@@ -1,3 +1,11 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return '%d run, 0 failed' % self.runCount
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -19,6 +27,8 @@ class WasRun(TestCase):
         self.log = 'setUp '
     def testMethod(self):
         self.log = self.log + 'testMethod '
+    def testBrokenMethod(self):
+        raise Exception
     def tearDown(self):
         self.log = self.log + 'tearDown '
 
@@ -31,14 +41,11 @@ class TestCaseTest(TestCase):
         test = WasRun('testMethod')
         result = test.run()
         assert('1 run, 0 failed' == result.summary())
-
-class TestResult:
-    def __init__(self):
-        self.runCount = 0
-    def testStarted(self):
-        self.runCount = self.runCount + 1
-    def summary(self):
-        return '%d run, 0 failed' % self.runCount
+    def testFailedResult(self):
+        test = WasRun('testBrokenMethod')
+        result = test.run()
+        assert('1 run, 1 failed' == result.summary())
 
 TestCaseTest('testTemplateMethod').run()
 TestCaseTest('testResult').run()
+# TestCaseTest('testFailedResult').run()
